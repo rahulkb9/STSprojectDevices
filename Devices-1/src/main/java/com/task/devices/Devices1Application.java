10ade8e2daf5c9748ee301d3e4a0deb09f30474b
vvdn <rahulkumar.baraik@vvdntech.in>
2022-10-09 17:49:02 +0530

    test commit.

---
+++ Devices-1/src/main/java/com/task/devices/Devices1Application.java
@@ -0,0 +1,13 @@
+package com.task.devices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devices1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devices1Application.class, args);
+	}
+
+}
